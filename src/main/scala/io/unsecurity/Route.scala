261bb0d6fdce955a80b0c68c6f3b236172911df6
Kaare Nilsen <kaare.nilsen@arktekk.no>
2018-10-19 12:51:51 +0200

    target

--- src/main/scala/io/unsecurity/Route.scala
+++ src/main/scala/io/unsecurity/Route.scala
@@ -20,3 +20,18 @@ class SafeRoute extends Route {
 
 }
 
+
+/*
+unsecurity
+    .safe|unsafe
+    .route("root" / 'id[String])
+    .queryParams?(param[List[String]("names)])
+    .consumes?(if requesttype with body)[MyRequest](application/json, appliation/xml)
+    .produces[MyResponse](application/json)
+    .authorization?((HasRole("admin) && HasAcessToParam("knut")))
+    .POST{ (body: BalleRequest, knut: Int, names: List[String]) =>
+      //body
+    }
+
+ */
+
