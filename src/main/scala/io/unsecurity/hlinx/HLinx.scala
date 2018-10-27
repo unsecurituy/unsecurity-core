12128684f7ca327954d611b5d9535ab7104eac5f
Eirik Meland <eirik.meland@arktekk.no>
2018-10-27 16:17:08 +0200

    simple test of variable fragment

--- src/main/scala/io/unsecurity/hlinx/HLinx.scala
+++ src/main/scala/io/unsecurity/hlinx/HLinx.scala
@@ -17,41 +17,60 @@ object HLinx {
   case class Param[A: ParamConverter](name: String)
 
   sealed trait LinkFragment[A <: HList] {
+    def staticFragments: Vector[String]
+
+    def matches(s: String): Boolean = {
+      val (parentMatch, leftOvers) = internalMatches(split(s))
+      parentMatch && leftOvers.isEmpty
+    }
+
+    def internalMatches(fs: Vector[String]): (Boolean, Vector[String])
+
+    def matchesStatic(fs: Vector[String]): (Boolean, Vector[String]) = {
+      val zipped: Vector[(String, String)] = fs zip staticFragments
+      if (zipped.size < staticFragments.size) (false, fs.drop(zipped.size))
+      else {
+        val isMatch = zipped.forall { case (a, b) => a == b }
+        (isMatch, if (isMatch) fs.drop(zipped.size) else Vector.empty)
+      }
+    }
+
     protected def split(s: String): Vector[String] =
       s.split("/").toVector.filter(_.nonEmpty)
   }
 
   val Root = StaticFragment(Vector.empty)
 
-  case class StaticFragment(fragments: Vector[String]) extends LinkFragment[HNil] {
+  case class StaticFragment(staticFragments: Vector[String]) extends LinkFragment[HNil] {
     def /(s: String): StaticFragment =
-      StaticFragment(fragments ++ split(s))
+      StaticFragment(staticFragments ++ split(s))
 
     def /[A](p: Param[A]): VarFragment[A, HNil] =
       VarFragment(this, Vector.empty, p)
 
-    def matches(s: String): Boolean = {
-      val (parentMatch, leftovers) = internalMatches(split(s))
-      parentMatch && leftovers.isEmpty
-    }
-
-    def internalMatches(fs: Vector[String]): (Boolean, Vector[String]) = {
-      val g = fs zip fragments
-
-      if (g.size < fragments.size) (false, fs.drop(g.size))
-      else {
-        val isMatch = g.forall { case (a, b) => a == b }
-        (isMatch, if (isMatch) fs.drop(g.size) else Vector.empty)
-      }
-    }
-
+    override def internalMatches(fs: Vector[String]): (Boolean, Vector[String]) =
+      matchesStatic(fs)
   }
 
-  case class VarFragment[A, B <: HList](parent: LinkFragment[B], static: Vector[String], param: Param[A]) extends LinkFragment[A :: B] {
+  case class VarFragment[A, B <: HList](parent: LinkFragment[B], staticFragments: Vector[String], param: Param[A]) extends LinkFragment[A :: B] {
     def /(s: String): VarFragment[A, B] =
-      VarFragment(parent, static ++ s.split("/"), param)
+      VarFragment(parent, staticFragments ++ s.split("/"), param)
 
     def /[C](p: Param[C]): VarFragment[C, A :: B] =
       VarFragment(this, Vector.empty, p)
+
+    override def internalMatches(fs: Vector[String]): (Boolean, Vector[String]) = {
+      val (parentMatches, leftOvers) = parent.internalMatches(fs)
+
+      if (parentMatches) {
+        if (leftOvers.nonEmpty) {
+          matchesStatic(leftOvers.drop(1))
+        } else {
+          (false, Vector.empty)
+        }
+      } else {
+        (false, Vector.empty)
+      }
+    }
   }
 }
--- src/test/scala/HLinxTest.scala
+++ src/test/scala/HLinxTest.scala
@@ -4,7 +4,6 @@ import org.scalatest.FunSpec
 class HLinxTest extends FunSpec {
 
   describe("StaticFragment") {
-
     describe("Root / foo / bar") {
       val link: StaticFragment = Root / "foo" / "bar"
 
@@ -26,15 +25,41 @@ class HLinxTest extends FunSpec {
     }
   }
 
-//  test("match /api/fjon") {
-//    val link = Root / "api" / "fjon"
-//
-//    assert(link.matches("/api/fjon")._1)
-//  }
-//
-//  test("matching variable") {
-//    val link = Root / "api" / Param[String]("var1")
-//
-//    assert(link.matches("/api/fjon")._1)
-//  }
+  describe("VariableFragment") {
+    describe("Root / foo / Param[String](bar)") {
+      val link: VarFragment[String, HNil] = Root / "foo" / Param[String]("bar")
+
+      it("should match /foo/foo") {
+        assert(link.matches("/foo/foo"))
+      }
+
+      it("should match /foo/1") {
+        assert(link.matches("/foo/1"))
+      }
+
+      it("should not match /foo") {
+        assert(!link.matches("/foo"))
+      }
+    }
+
+    describe("Root / foo / Param[String](bar) / baz") {
+      val link: VarFragment[String, HNil] = Root / "foo" / Param[String]("bar") / "baz"
+
+      it("should match /foo/1/baz") {
+        assert(link.matches("/foo/1/baz"))
+      }
+
+      it("should not match /foo/1") {
+        assert(!link.matches("/foo/1"))
+      }
+
+      it("should not match /foo/1/foo") {
+        assert(!link.matches("/foo/1/foo"))
+      }
+
+      it("should not match /foo/1/baz/baz") {
+        assert(!link.matches("/foo/1/baz/baz"))
+      }
+    }
+  }
 }
