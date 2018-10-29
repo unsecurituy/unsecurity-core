21fabc062543620a1707be07c9b101d3872ff11e
Kaare Nilsen <kaare.nilsen@arktekk.no>
2018-10-29 15:01:30 +0100

    something old, something borrowed and something new

--- src/main/scala/io/unsecurity/Unsecurity.scala
+++ src/main/scala/io/unsecurity/Unsecurity.scala
@@ -1,29 +1,31 @@
 package io.unsecurity
 
 import cats.effect.IO
+import io.unsecurity.hlinx.HLinx.{HList, LinkFragment}
 import no.scalabin.http4s.directives.Directive
+import io.unsecurity.hlinx.HLinx._
 
 object Unsecurity {
   def safe[F[_]]: Safe[F]     = ???
   def unsafe[F[_]]: Unsafe[F] = ???
 }
 
-class Route[F[_]](
-    path: String,
+class CompleteRoute[F[_], A, B <: HList](
+    path: LinkFragment[B],
     queryParams: List[String],
     accepts: List[String],
     produces: List[String],
     authorization: Option[String],
     method: String,
-    f: () => String
+    f: () => Directive[F, A]
 )
 
 class Unsafe[F[_]] {
-  def route(path: String) = ???
+  def route[A <: HList](path: LinkFragment[A]): UnSafeRoute[F] = ???
 }
 
 class Safe[F[_]] {
-  def route(path: String): SafeRoute[F] = ???
+  def route[A <: HList](path: LinkFragment[A]): SafeRoute[F] = ???
 }
 
 class SafeRoute[F[_]](
@@ -33,11 +35,24 @@ class SafeRoute[F[_]](
     produces: List[String],
     authorization: Option[String]
 ) extends Safe {
-  def queryParams(param: String): SafeRoute[F]                       = ???
-  def accepts(mediaType: String*): SafeRoute[F]                      = ???
-  def produces(mediaType: String*): SafeRoute[F]                     = ???
-  def authorization[A](authRule: SecurityPredicate[A]): SafeRoute[F] = ???
-  def GET[A](f: () => Directive[F, A]): Route[F]                     = ???
+  def queryParams(param: String): SafeRoute[F]                             = ???
+  def accepts(mediaType: String*): SafeRoute[F]                            = ???
+  def produces(mediaType: String*): SafeRoute[F]                           = ???
+  def authorization[A](authRule: SecurityPredicate[A]): SafeRoute[F]       = ???
+  def GET[A, B <: HList](f: () => Directive[F, A]): CompleteRoute[F, A, B] = ???
+}
+
+class UnSafeRoute[F[_]](
+    path: String,
+    queryParams: List[String],
+    accepts: List[String],
+    produces: List[String],
+    authorization: Option[String]
+) extends Safe {
+  def queryParams(param: String): SafeRoute[F]                             = ???
+  def accepts(mediaType: String*): SafeRoute[F]                            = ???
+  def produces(mediaType: String*): SafeRoute[F]                           = ???
+  def GET[A, B <: HList](f: () => Directive[F, A]): CompleteRoute[F, A, B] = ???
 }
 
 class SecurityPredicate[-A](predicate: A => Boolean) {
@@ -51,10 +66,10 @@ class SecurityPredicate[-A](predicate: A => Boolean) {
 }
 
 object Test {
-  val aRoute: Route[IO] = Unsecurity
+  val aRoute: CompleteRoute[IO, String, Int :: HNil] = Unsecurity
     .safe[IO]
-    .route("")
-    .GET[String] { () =>
+    .route(Root / "balle" / Param[Int]("knut"))
+    .GET[String, Int :: HNil] { () =>
       Directive.success("Hello")
     }
 }
