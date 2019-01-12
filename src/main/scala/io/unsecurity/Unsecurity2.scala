e6ae0010a6c4cc1afbf0373b4c5020f0d27372ef
Eirik Meland <eirik.meland@arktekk.no>
2019-01-12 14:40:45 +0100

    lock F at abstract class unsecurity

--- src/main/scala/io/unsecurity/Unsecurity2.scala
+++ src/main/scala/io/unsecurity/Unsecurity2.scala
@@ -1,7 +1,7 @@
 package io.unsecurity
 
-import cats.{Applicative, Monad}
-import cats.effect.{IO, Sync}
+import cats.Applicative
+import cats.effect.Sync
 import io.circe.{Decoder, Encoder}
 import io.unsecurity.Unsecure.PathMatcher
 import io.unsecurity.hlinx.HLinx
@@ -10,53 +10,53 @@ import no.scalabin.http4s.directives.Conditional.ResponseDirective
 import no.scalabin.http4s.directives.{Directive, RequestDirectives}
 import org.http4s.{EntityDecoder, EntityEncoder, Method}
 
-object Unsecurity2 {
+abstract class Unsecurity2[F[_]: Sync : Applicative] {
 
   object Read {
     case object No extends Read[Unit] {
-      override def entityDecoder[F[_]: Sync]: EntityDecoder[F, Unit] = ???
+      override def entityDecoder: EntityDecoder[F, Unit] = ???
     }
     def json[A: Decoder]: Read[A] = new Read[A] {
-      override def entityDecoder[F[_]: Sync]: EntityDecoder[F, A] = ???
+      override def entityDecoder: EntityDecoder[F, A] = ???
     }
   }
   sealed trait Read[A] {
-    def entityDecoder[F[_]: Sync]: EntityDecoder[F, A]
+    def entityDecoder: EntityDecoder[F, A]
   }
 
   object Write {
     case object No extends Write[Unit] {
-      override def entityEncoder[F[_]: Applicative]: EntityEncoder[F, Unit] =
+      override def entityEncoder: EntityEncoder[F, Unit] =
         ???
     }
     def json[A: Encoder]: Write[A] = new Write[A] {
-      override def entityEncoder[F[_]: Applicative]: EntityEncoder[F, A] =
+      override def entityEncoder: EntityEncoder[F, A] =
         org.http4s.circe.jsonEncoderOf[F, A]
     }
   }
   sealed trait Write[A] {
-    def entityEncoder[F[_]: Applicative]: EntityEncoder[F, A]
+    def entityEncoder: EntityEncoder[F, A]
   }
 
-  trait Safe[F[_], C, W] extends Completable[F, C, W] {
-    def authorization(predicate: C => Boolean): Completable[F, C, W]
-    override def resolve[C2](f: C => F[C2]): Safe[F, C2, W]
+  trait Safe[C, W] extends Completable[C, W] {
+    def authorization(predicate: C => Boolean): Completable[C, W]
+    override def resolve[C2](f: C => F[C2]): Safe[C2, W]
   }
 
-  trait Authenticator[F[_], A] {
-    def secure[P <: HList, R, W](endpoint: Endpoint[P, R, W]): Safe[F, (P, R, A), W]
-    def unsecure[P <: HList, R, W](endpoint: Endpoint[P, R, W]): Completable[F, (P, R), W] =
+  trait Authenticator[A] {
+    def secure[P <: HList, R, W](endpoint: Endpoint[P, R, W]): Safe[(P, R, A), W]
+    def unsecure[P <: HList, R, W](endpoint: Endpoint[P, R, W]): Completable[(P, R), W] =
       ???
   }
 
-  class UnsecureAuthenticator[F[_]: Monad: Sync] extends Authenticator[F, Unit] with RequestDirectives[F] {
-    override def secure[P <: HList, R, W](endpoint: Endpoint[P, R, W]): Safe[F, (P, R, Unit), W] = ???
-    override def unsecure[P <: HList, R, W](endpoint: Endpoint[P, R, W]): Completable[F, (P, R), W] = {
-      MyCompletable[F, (P, R), W](
+  class UnsecureAuthenticator extends Authenticator[Unit] with RequestDirectives[F] {
+    override def secure[P <: HList, R, W](endpoint: Endpoint[P, R, W]): Safe[(P, R, Unit), W] = ???
+    override def unsecure[P <: HList, R, W](endpoint: Endpoint[P, R, W]): Completable[(P, R), W] = {
+      MyCompletable[(P, R), W](
         pathMatcher = Unsecure.createPathMatcher[F, P](endpoint.path).asInstanceOf[PathMatcher[F, Any]],
         methodMap = Map(
           endpoint.method -> { pp: P =>
-            implicit val entityDecoder: EntityDecoder[F, R] = endpoint.read.entityDecoder[F]
+            implicit val entityDecoder: EntityDecoder[F, R] = endpoint.read.entityDecoder
             for {
               r <- request.bodyAs[F, R]
             } yield {
@@ -69,31 +69,31 @@ object Unsecurity2 {
     }
   }
 
-  trait Completable[F[_], C, W] {
-    def run(f: C => Directive[F, W]): Complete[F]
-    def resolve[C2](f: C => F[C2]): Completable[F, C2, W]
+  trait Completable[C, W] {
+    def run(f: C => Directive[F, W]): Complete
+    def resolve[C2](f: C => F[C2]): Completable[C2, W]
   }
 
-  case class MyCompletable[F[_], C, W](
+  case class MyCompletable[C, W](
       pathMatcher: PathMatcher[F, Any],
       methodMap: Map[Method, Any => Directive[F, C]],
       entityEncoder: EntityEncoder[F, W]
-  ) extends Completable[F, C, W] {
-    override def run(f: C => Directive[F, W]): Complete[F] = {
+  ) extends Completable[C, W] {
+    override def run(f: C => Directive[F, W]): Complete = {
 
       ???
     }
-    override def resolve[C2](f: C => F[C2]): Completable[F, C2, W] = ???
+    override def resolve[C2](f: C => F[C2]): Completable[C2, W] = ???
   }
 
-  trait Complete[F[_]] {
-    def ||(next: Complete[F]): Complete[F]
+  trait Complete {
+    def ||(next: Complete): Complete
   }
 
-  case class MyComplete[F[_]](pathMatcher: PartialFunction[String, Any],
+  case class MyComplete(pathMatcher: PartialFunction[String, Any],
                               methodMap: Map[Method, Any => ResponseDirective[F]])
-      extends Complete[F] {
-    override def ||(next: _root_.io.unsecurity.Unsecurity2.Complete[F]): _root_.io.unsecurity.Unsecurity2.Complete[F] =
+      extends Complete {
+    override def ||(next: Complete): Complete =
       ???
   }
 
@@ -102,6 +102,7 @@ object Unsecurity2 {
                                               read: Read[R] = Read.No,
                                               write: Write[W] = Write.No)
 
+/*
   val auth: Authenticator[IO, String] = ???
 
   import auth._
@@ -123,5 +124,6 @@ object Unsecurity2 {
     ))
     .authorization(_ => true)
     .run(_ => Directive.success(42))
+*/
 
 }
--- src/test/scala/io/unsecurity/Main2.scala
+++ src/test/scala/io/unsecurity/Main2.scala
@@ -1,7 +1,5 @@
 package io.unsecurity
 import cats.effect.{ExitCode, IO, IOApp}
-import io.unsecurity.Unsecurity2._
-import io.unsecurity.hlinx.HLinx
 import io.unsecurity.hlinx.HLinx.Root
 import no.scalabin.http4s.directives.Directive
 import org.http4s.Method
@@ -9,18 +7,21 @@ import org.http4s.Method
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main2 extends IOApp {
-  import Unsecurity2._
+  val unsecurity2: Unsecurity2[IO] = new Unsecurity2[IO] {}
+  import unsecurity2._
 
   val server: Server[IO] = Server[IO](
     port = 8088,
     host = "0.0.0.0"
   )
 
-  val auth = new UnsecureAuthenticator[IO]
+
+
+  val auth = new unsecurity2.UnsecureAuthenticator
 
   import auth._
 
-  val helloWorld: Unsecurity2.Complete[IO] =
+  val helloWorld =
     unsecure(
       Endpoint(
         method = Method.GET,
